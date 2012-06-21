a884e1fdb0d728cedff6e52f963030a3ec8c60df
Glen Holcomb <MentalLabor@gmail.com>
2012-06-21 09:45:51 -0600

    Document distribution added.

--- lib/google_apps/atom/atom.rb
+++ lib/google_apps/atom/atom.rb
@@ -2,9 +2,24 @@ require 'libxml'
 require 'openssl'
 require 'base64'
 
+class String
+  def camel_up
+    self.split('_').map(&:capitalize).join('')
+  end
+end
+
 module GoogleApps
   module Atom
     include LibXML
+
     HASH_FUNCTION = "SHA-1"
+    DOCUMENTS = %w(user export group group_member message_attributes public_key)
+
+    # The idea is to make document distribution more dynamic.
+    # Might be pointless but it's here for now.
+    DOCUMENTS.each do |doc|
+      eval "def #{doc}\n  #{doc.camel_up}.new\nend"
+      module_function doc.to_sym
+    end
   end
 end
