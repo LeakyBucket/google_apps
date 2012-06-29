85c2816d760456fb51b7d8c277aebe4e3129a945
Glen Holcomb <DamnBigMan@gmail.com>
2012-06-28 20:34:05 -0600

    Feed processing is ugly as hell, but appears to be working.  Needs more thorough tests though.

--- lib/google_apps/atom/feed.rb
+++ lib/google_apps/atom/feed.rb
@@ -14,14 +14,20 @@ module GoogleApps
         @items = entries_from document: @xml, type: @xml.to_s.match(TYPE_MATCH).captures[0], entry_tag: 'entry'
       end
 
+      # TODO: This fucking stinks like no ones business.
       def entries_from(properties)
         type = properties[:type].downcase.match(/(\w*?)s$|$/).captures[0].to_sym
         #type = properties[:type]
 
         properties[:document].root.inject([]) do |results, entry|
           if entry.name == properties[:entry_tag]
-            xml = entry.children.inject('') { |xml, entry| xml + entry.to_s }
-            results << Atom.send(type, xml)
+            doc = Atom.send type
+            doc.document.root = create_node type: 'apps:entry'
+            add_namespaces doc.document.root, atom: 'http://www.w3.org/2005/Atom', apps: 'http://schemas.google.com/apps/2006'
+            entry.children.each do |child|
+              doc.document.root << doc.document.import(child)
+            end
+            results << doc
           end
           #results << Atom.send(type, entry) if entry.name == properties[:entry_tag]
           results
