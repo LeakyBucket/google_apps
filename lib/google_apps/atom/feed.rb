9f82db32ac2ec6a678258c6089a237ee8a0cf358
Glen Holcomb <DamnBigMan@gmail.com>
2012-07-03 12:47:31 -0600

    Documentation

--- lib/google_apps/atom/feed.rb
+++ lib/google_apps/atom/feed.rb
@@ -28,11 +28,26 @@ module GoogleApps
       end
 
 
+      # node_to_ary converts a Atom::XML::Node to an array.
+      #
+      # node_to_ary node
+      #
+      # node_to_ary returns the string representation of the
+      # given node split on \n.
       def node_to_ary(node)
         node.to_s.split("\n")
       end
 
 
+      # new_doc creates a new Atom document from the data
+      # provided in the feed.  new_doc takes a type, an
+      # array of content to be placed into the document
+      # as well as an array of filters.
+      #
+      # new_doc 'user', content_array, ['apps:']
+      #
+      # new_doc returns an GoogleApps::Atom document of the
+      # specified type.
       def new_doc(type, content_array, filters)
         content_array = filters.inject([]) do |content, filter|
           content << grab_elements(content_array, filter)
@@ -43,11 +58,28 @@ module GoogleApps
       end
 
 
+      # grab_elements applies the specified filter to the
+      # provided array.  Google's feed provides a lot of data
+      # that we don't need in an entry document.
+      #
+      # grab_elements content_array, 'apps:'
+      #
+      # grab_elements returns an array of items from content_array
+      # that match the given filter.
       def grab_elements(content_array, filter)
         content_array.grep(Regexp.new filter)
       end
 
 
+      # entry_wrap adds atom:entry opening and closing tags
+      # to the provided content_array and the beginning and
+      # end.
+      #
+      # entry_wrap content_array
+      #
+      # entry_wrap returns an array with an opening atom:entry
+      # element prepended to the front and a closing atom:entry
+      # tag appended to the end.
       def entry_wrap(content_array)
         content_array.unshift(Atom::ENTRY_TAG[0]).push(Atom::ENTRY_TAG[1])
       end
