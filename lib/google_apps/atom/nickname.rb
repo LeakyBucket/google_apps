6a39bea799739c10f7bafbc581b6a5815283d434
Glen Holcomb <DamnBigMan@gmail.com>
2012-07-13 09:28:02 -0600

    Cleanup

--- lib/google_apps/atom/nickname.rb
+++ lib/google_apps/atom/nickname.rb
@@ -35,7 +35,9 @@ module GoogleApps
       #
       # user= returns the new username value
       def user=(username)
-        @user.nil? ? set_user(username) : change_user(username)
+        @user ? find_and_update(@document, '//apps:login', userName: [@user, username]) : create('login', username)
+
+        @user = username
       end
 
 
@@ -49,6 +51,12 @@ module GoogleApps
       private
 
 
+      # create adds the specified node to @document.  It takes
+      # a type and a value as arguments.
+      #
+      # create 'nickname', 'Bob'
+      #
+      # create returns a parsed copy of the document.
       def create(type, value)
         case type
         when 'nickname'
@@ -75,36 +83,6 @@ module GoogleApps
         create_node type: 'atom:category', attrs: Atom::CATEGORY[:nickname]
       end
 
-
-      # set_user adds an apps:login node to the underlying
-      # XML document and sets @user.  It takes a username
-      # (current/default username) in string form for its
-      # argument.
-      #
-      # set_user 'bob'
-      #
-      # set_user returns the new user value.
-      def set_user(username)
-        @document.root << create_node(type: 'apps:login', attrs: [['userName', username]])
-
-        @user = username
-      end
-
-
-      # change_user changes the userName attribute for the
-      # apps:login node in the underlying XML document. It
-      # takes a username (Email format) in string form.
-      #
-      # change_user 'bob@work.com'
-      #
-      # change_user returns the new user value.
-      def change_user(username)
-        @document.root.each do |node|
-          node.attributes['userName'] = username if node.attributes['userName'] == @user
-        end
-
-        @user = username
-      end
 
       # :nodoc:
       def change_element(type, value)
