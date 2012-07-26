dc52ff501d02314627cde9c0aea3f5af44c519cc
Glen Holcomb <DamnBigMan@gmail.com>
2012-07-26 13:07:01 -0600

    Work on GoogleApps::DocumentHandler

--- lib/google_apps/document_handler.rb
+++ lib/google_apps/document_handler.rb
@@ -1,13 +1,46 @@
 module GoogleApps
   class DocumentHandler
-    attr_reader :format
+    attr_accessor :format
 
     def initialize(args)
-      @format = args[:format]
+      set_format args[:format]
     end
     
+
+    # doc_from_string creates a document of the specified format
+    # from the given string.
     def doc_from_string(text)
-      
+      case @format
+      when :atom, :xml
+        Atom::XML::Document.string(text)
+      end
+    end
+
+
+    # format= sets the format for the DocumentHandler
+    def format=(format)
+      set_format format
+    end
+
+
+
+    private
+
+
+    # look_up_doc_types returns a list of document types the
+    # library supports in the current format.
+    def look_up_doc_types
+      case @format
+      when :atom, :xml
+        Atom::DOCUMENTS
+      end
+    end
+
+
+    # set_format Sets @format and @documents
+    def set_format(format)
+      @format = format
+      @documents = look_up_doc_types
     end
   end
 end
--- spec/document_handler_spec.rb
+++ spec/document_handler_spec.rb
@@ -4,14 +4,58 @@ describe "GoogleApps::DocumentHandler" do
   let (:handler) { GoogleApps::DocumentHandler.new format: :atom }
 
   describe "#new" do
-    it "Sets @type to the type of document to be processed" do
+    it "Sets @format to the format of the document to be processed" do
       handler.format.should == :atom
     end
-
-    it "Includes the proper modules based on @type"
   end
 
   describe "#doc_from_string" do
-    it "Returns a document of the given type using the given string"
+    it "Returns a XML document when given xml and @format is :atom" do
+      handler.doc_from_string(finished_export).should be_a LibXML::XML::Document
+    end
+
+    it "Returns a XML document when given xml and @format is :xml" do
+      handler.format = :xml
+      handler.doc_from_string(finished_export).should be_a LibXML::XML::Document
+    end
+  end
+
+  describe "#format=" do
+    it "Changes the format" do
+      handler.format = :xml
+
+      handler.format.should == :xml
+    end
+
+    it "Rebuilds the @documents list" do
+      handler.format = :xml
+
+      handler.instance_eval { @documents }.should == GoogleApps::Atom::DOCUMENTS
+    end
+  end
+
+  describe "#look_up_doc_types" do
+    it "Returns a list of all Atom documents when @format is :atom" do
+      handler.send(:look_up_doc_types).should == GoogleApps::Atom::DOCUMENTS
+    end
+
+    it "Returns a list of all Atom documents when @format is :xml" do
+      handler.format = :xml
+      handler.send(:look_up_doc_types).should == GoogleApps::Atom::DOCUMENTS
+    end
+  end
+
+  describe "#set_format" do
+    it "Sets @format to the given value" do
+      handler.send(:set_format, :xml)
+
+      handler.format.should == :xml
+    end
+
+    it "Sets the @document list" do
+      handler.send(:set_format, :xml)
+
+      handler.instance_eval { @documents }.should == GoogleApps::Atom::DOCUMENTS
+    end
   end
 end
