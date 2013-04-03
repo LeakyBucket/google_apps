d5eb8eee047d064e47ec4296739064ae5142d61b
Will Read <will.read@gmail.com>
2013-04-02 18:33:11 -0700

    Create users through the provisioning API

--- lib/google_apps/provisioning_api/user.rb
+++ lib/google_apps/provisioning_api/user.rb
@@ -1,10 +1,9 @@
 class GoogleApps
   module ProvisioningApi
     class User
-      attr_accessor :login, :given_name, :family_name, :storage_quota, :suspended
-      alias_method :last_name, :family_name
-      alias_method :first_name, :given_name
+      attr_accessor :login, :first_name, :last_name, :storage_quota, :suspended, :admin
       alias_method :suspended?, :suspended
+      alias_method :admin?, :admin
 
       def initialize(attrs = {})
         attrs.each_pair { |name, value| self.send("#{name}=", value) }
@@ -26,6 +25,32 @@ class GoogleApps
         end
       end
 
+      def self.create(attrs = {})
+        document = <<-XML
+<atom:entry xmlns:atom='http://www.w3.org/2005/Atom'
+            xmlns:apps='http://schemas.google.com/apps/2006'>
+  <apps:property name="password" value="#{attrs[:password]}"/>
+  <apps:property name="hashFunction" value="SHA-1"/>
+  <apps:property name="userEmail" value="#{attrs[:email]}"/>
+  <apps:property name="firstName" value="#{attrs[:first_name]}"/>
+  <apps:property name="lastName" value="#{attrs[:last_name]}"/>
+  <apps:property name="isAdmin" value="#{attrs[:admin]}"/>
+  <apps:property name="isSuspended" value="#{attrs[:suspended]}"/>
+</atom:entry>
+        XML
+        begin
+          response = GoogleApps.client.make_request(:post, user_url, body: document, headers: {'Content-type' => 'application/atom+xml'})
+          atom_feed = REXML::Document.new(response.body)
+          from_entry(atom_feed.elements['//entry'])
+        rescue
+          nil
+        end
+      end
+
+      def update(attrs = {})
+
+      end
+
       private
       def self.user_url
         "https://apps-apis.google.com/a/feeds/#{GoogleApps.client.domain}/user/2.0"
@@ -34,8 +59,10 @@ class GoogleApps
       def self.from_entry(atom_entry)
         new(
             :login => atom_entry.elements["apps:login"].attribute("userName").value,
-            :given_name => atom_entry.elements["apps:name"].attribute("givenName").value,
-            :family_name => atom_entry.elements["apps:name"].attribute("familyName").value,
+            :admin => atom_entry.elements["apps:login"].attribute("admin").value == 'true',
+            :suspended => atom_entry.elements["apps:login"].attribute("suspended").value == 'true',
+            :first_name => atom_entry.elements["apps:name"].attribute("givenName").value,
+            :last_name => atom_entry.elements["apps:name"].attribute("familyName").value,
             :storage_quota => atom_entry.elements["apps:quota"].attribute("limit").value.to_i,
         )
       end
--- spec/google_apps/provisioning_api/user_spec.rb
+++ spec/google_apps/provisioning_api/user_spec.rb
@@ -1,51 +1,104 @@
 require 'spec_helper'
 
-describe GoogleApps::ProvisioningApi::User do
-  before { GoogleApps.client = Klient.new('example.com') }
+class GoogleApps
+  module ProvisioningApi
 
-  describe ".find" do
-    context 'when the user is found' do
-      before do
-        stub_request(:get, "https://apps-apis.google.com/a/feeds/example.com/user/2.0/mmcfly").
-            to_return(:status => 200, :body => File.read('spec/fixture_xml/user_response.xml'))
+    describe User do
+      before { GoogleApps.client = Klient.new('example.com') }
+
+      describe ".find" do
+        context 'when the user is found' do
+          before do
+            stub_request(:get, "https://apps-apis.google.com/a/feeds/example.com/user/2.0/mmcfly").
+                to_return(:status => 200, :body => File.read('spec/fixture_xml/user_response.xml'))
+          end
+
+          it 'returns the user' do
+            user = User.find('mmcfly')
+            user.should be_a(User)
+            user.login.should == 'mmcfly'
+            user.first_name.should == "Marty"
+            user.last_name.should == "McFly"
+            user.storage_quota.should == 25600
+            user.should_not be_suspended
+            user.should be_admin
+          end
+        end
+
+        context 'when the user is not found' do
+          before do
+            stub_request(:get, "https://apps-apis.google.com/a/feeds/example.com/user/2.0/not-found-user").
+                to_return(:status => 404)
+          end
+
+          it 'returns nil' do
+            user = User.find('not-found-user')
+            user.should be_nil
+          end
+        end
       end
 
-      it 'returns the user' do
-        user = GoogleApps::ProvisioningApi::User.find('mmcfly')
-        user.should be_a(GoogleApps::ProvisioningApi::User)
-        user.login.should == 'mmcfly'
-        user.first_name.should == "Marty"
-        user.last_name.should == "McFly"
-        user.storage_quota.should == 25600
-        user.should_not be_suspended
+      describe ".all" do
+        context 'when there are users' do
+          before do
+            stub_request(:get, "https://apps-apis.google.com/a/feeds/example.com/user/2.0").
+                to_return(:status => 200, :body => File.read('spec/fixture_xml/users_response.xml'))
+          end
+
+          it 'returns an array of users' do
+            users = User.all
+            users.count.should == 2
+            users.first.should be_a(User)
+          end
+        end
+      end
+
+      describe ".create" do
+        describe '#add_user' do
+          let (:user_doc) {
+            <<-XML
+<atom:entry xmlns:atom='http://www.w3.org/2005/Atom'
+            xmlns:apps='http://schemas.google.com/apps/2006'>
+  <apps:property name="password" value="zonkey"/>
+  <apps:property name="hashFunction" value="SHA-1"/>
+  <apps:property name="userEmail" value="liz@example.com"/>
+  <apps:property name="firstName" value="Liz"/>
+  <apps:property name="lastName" value="Smith"/>
+  <apps:property name="isAdmin" value="true"/>
+  <apps:property name="isSuspended" value="false"/>
+</atom:entry>
+            XML
+          }
+
+          it "returns a user on successful POST" do
+
+            stub_request(:post, "https://apps-apis.google.com/a/feeds/example.com/user/2.0").
+                with(:body => user_doc, headers: {'Content-type' => 'application/atom+xml'}).
+                to_return(status: 200, body: File.read('spec/fixture_xml/user_response.xml'))
+            user = User.create(email: 'liz@example.com',
+                        password: 'zonkey',
+                        first_name: 'Liz',
+                        last_name: 'Smith',
+                        admin: true,
+                        suspended: false
+            )
+
+            user.login.should == 'mmcfly'
+            #user.password.should be_hashed
+            user.first_name.should == 'Marty'
+            user.last_name.should == 'McFly'
+            user.should be_admin
+            user.should_not be_suspended
+          end
+
+          it "returns nil on unsuccessful POST" do
+            stub_request(:post, "https://apps-apis.google.com/a/feeds/example.com/user/2.0").
+                to_return(status: 404)
+            User.create(email: 'liz@example.com')
+          end
+        end
       end
     end
 
-    context 'when the user is not found' do
-      before do
-        stub_request(:get, "https://apps-apis.google.com/a/feeds/example.com/user/2.0/not-found-user").
-            to_return(:status => 404)
-      end
-
-      it 'returns nil' do
-        user = GoogleApps::ProvisioningApi::User.find('not-found-user')
-        user.should be_nil
-      end
-    end
-  end
-
-  describe ".all" do
-    context 'when there are users' do
-      before do
-        stub_request(:get, "https://apps-apis.google.com/a/feeds/example.com/user/2.0").
-            to_return(:status => 200, :body => File.read('spec/fixture_xml/users_response.xml'))
-      end
-
-      it 'returns an array of users' do
-        users = GoogleApps::ProvisioningApi::User.all
-        users.count.should == 2
-        users.first.should be_a(GoogleApps::ProvisioningApi::User)
-      end
-    end
   end
 end
